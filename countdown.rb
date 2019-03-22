9f52fa042e93e00956250eda79348871c680d1cf
minh nguyen <imnotminh@gmail.com>
2019-03-22 01:34:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
-def countdown(x)
+def countdown(number)
   y=0
-  while y < x
-     puts "#{x} SECOND(S)!" 
-     x-=1
+  while y < number
+     puts "#{number} SECOND(S)!" 
+     number-=1
   end
   puts "HAPPY NEW YEAR!"
 end
