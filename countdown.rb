834ef1ee9c7688e0c8db5431913df09afa8db4e7
minh nguyen <imnotminh@gmail.com>
2019-03-22 02:05:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,14 @@ def countdown(number)
   end
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+
+  y=0
+  while y <= number
+     puts "#{number} SECOND(S)!" 
+     number-=1
+     sleep 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
